c6dd43af0bda856741452fdedd4a85a3cbefa2d6
ttmtu2003 <ttmtu2003@gmail.com>
2022-11-05 00:37:52 -0700

    remove course

---
+++ client/src/application/model/CourseModel.java
@@ -0,0 +1,15 @@
+package application.model;
+
+
+public class CourseModel {
+	private String courseName;
+	
+	public String getCourseName() {
+		return courseName;
+	}
+	
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+}
